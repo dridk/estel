2b2a0048a245d9c5510cd241ab1887ac8e525222
sacha <sacha@labsquare.org>
2012-08-24 16:29:26 +0200

    Fix #34 for Simulation Dialog

--- widget/simulationdialog.cpp
+++ widget/simulationdialog.cpp
@@ -3,61 +3,83 @@
 #include <QDateTime>
 #include <QApplication>
 #include <QFileDialog>
+
+//============== SIMULATION THREAD ==============
+SimulationThread::SimulationThread(LifeEngine *engine)
+{
+    mEngine = engine;
+}
+
+
+
+void SimulationThread::setParams(int totalStep, const QString &destination)
+{
+    mTotalStep = totalStep;
+    mDestination = destination;
+}
+
+void SimulationThread::run()
+{
+    int i = 0;
+    QDateTime beginTime = QDateTime::currentDateTime();
+    while (i<mTotalStep)
+    {
+        QDateTime time;
+        time.fromTime_t(beginTime.toTime_t()-QDateTime::currentDateTime().toTime_t());
+
+        mEngine->step();
+
+        QString filename= "sim_"+QString::number(i)+".estel";
+        filename = mDestination+QDir::separator()+filename;
+
+        if(!mEngine->save(filename))
+            qDebug()<<"cannot save...";
+
+        qDebug()<<filename<<" saved";
+        ++i;
+        emit fileSaved(filename);
+        emit stepChanged(i);
+
+    }
+}
+//============== SIMULATION Dialog ==============
+
 SimulationDialog::SimulationDialog(LifeEngine *engine, QWidget *parent):
     QDialog(parent),
     ui(new Ui::SimulationDialog)
 {
     ui->setupUi(this);
     mEngine = engine;
+    mThread = new SimulationThread(engine);
 
     setWindowTitle("Simulation Player");
     connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(run()));
     connect(ui->toolButton,SIGNAL(clicked()),this,SLOT(setDestination()));
+    connect(mThread,SIGNAL(finished()),this,SLOT(finished()));
+    connect(mThread,SIGNAL(fileSaved(QString)),
+            ui->plainTextEdit,SLOT(appendPlainText(QString)));
+    connect(mThread,SIGNAL(stepChanged(int)),
+            ui->progressBar,SLOT(setValue(int)));
+    connect(ui->stopButton,SIGNAL(clicked()),mThread,SLOT(terminate()));
 
     ui->lineEdit->setText(qApp->applicationDirPath());
-
-
 }
 
 SimulationDialog::~SimulationDialog()
 {
     delete ui;
+    delete mThread;
 }
 
 void SimulationDialog::run()
 {
-    int totalStep = ui->spinBox->value();
-    int i = 0;
-    QDateTime beginTime = QDateTime::currentDateTime();
-    ui->progressBar->setRange(0,totalStep);
     ui->pushButton->setEnabled(false);
+    ui->progressBar->setRange(0,ui->spinBox->value());
+
+    mThread->setParams(ui->spinBox->value(), ui->lineEdit->text());
+    mThread->run();
 
 
-    while (i<totalStep)
-    {
-        QDateTime time;
-        time.fromTime_t(beginTime.toTime_t()-QDateTime::currentDateTime().toTime_t());
-
-        ui->popLabel->setText(QString::number(mEngine->population()));
-        ui->sizeLabel->setText(QString::number(sizeof(Life) * mEngine->population()));
-        mEngine->step();
-
-        QString filename= "sim_"+QString::number(i)+".estel";
-
-        filename = ui->lineEdit->text()+QDir::separator()+filename;
-
-        qDebug()<<filename;
-
-        if(mEngine->save(filename))
-            ui->plainTextEdit->appendPlainText(filename+" saved");
-
-
-
-        ++i;
-        ui->progressBar->setValue(i);
-
-    }
-    ui->pushButton->setEnabled(true);
 
 
 
@@ -66,13 +88,19 @@ void SimulationDialog::run()
 void SimulationDialog::setDestination()
 {
 
-    mDestination =
+    QString destination =
             QFileDialog::getExistingDirectory(this,
                                               "error",
                                               qApp->applicationDirPath());
 
 
-    ui->lineEdit->setText(mDestination);
+    ui->lineEdit->setText(destination);
 
 
 }
+
+void SimulationDialog::finished()
+{
+    ui->pushButton->setEnabled(false);
+}
+
--- widget/simulationdialog.h
+++ widget/simulationdialog.h
@@ -2,7 +2,31 @@
 #define SIMULATIONDIALOG_H
 
 #include <QDialog>
+#include <QThread>
 #include "lifeengine.h"
+
+
+class SimulationThread : public QThread
+{
+    Q_OBJECT
+public:
+    SimulationThread(LifeEngine * engine);
+    void setParams(int totalStep, const QString& destination);
+    void run();
+
+signals:
+    void fileSaved(const QString& filename);
+    void stepChanged(int step);
+
+
+private:
+    int mTotalStep;
+    QString mDestination;
+    LifeEngine * mEngine;
+
+};
+
+
 namespace Ui {
 class SimulationDialog;
 }
@@ -16,12 +40,16 @@ public:
     ~SimulationDialog();
 public slots:
     void run();
+
+protected slots:
     void setDestination();
+    void finished();
     
 private:
     Ui::SimulationDialog *ui;
     LifeEngine * mEngine;
-    QString mDestination;
+    SimulationThread * mThread;
+
 };
 
 #endif // SIMULATIONDIALOG_H
